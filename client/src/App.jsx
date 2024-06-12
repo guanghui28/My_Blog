3e010729faf31537ea5f3b04c7303a7b17dfb1cb
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-12 17:12:02 +0700

    create footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
@@ -19,6 +20,7 @@ const App = () => {
 				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/projects" element={<Projects />} />
 			</Routes>
+			<Footer />
 		</>
 	);
 };
