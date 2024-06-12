0937467d71c28292b1a31ce72e2a9ff306235d03
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-12 13:10:47 +0700

    create header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 const App = () => {
 	return (
 		<>
+			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
