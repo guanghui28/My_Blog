a2b860b8961c017bb2c7a52648d4117194dab2b8
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-12 19:16:20 +0700

    create signin API route & JWT

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,18 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
 	const { username, email, password } = req.body;
 
-	if (
-		!username ||
-		!email ||
-		!password ||
-		username === "" ||
-		email === "" ||
-		password === ""
-	) {
+	if (!username || !email || !password) {
 		return next(errorHandler(400, "All fields are required!"));
 	}
 
@@ -27,6 +21,41 @@ export const signup = async (req, res, next) => {
 	try {
 		await newUser.save();
 		res.status(201).json({ success: true, message: "Sign up successful" });
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const signin = async (req, res, next) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return next(errorHandler(400, "All fields are required!"));
+	}
+	try {
+		const validUser = await User.findOne({ email });
+		if (!validUser) {
+			return next(errorHandler(404, "Email or Password are incorrect"));
+		}
+
+		const isMatchedPassword = bcryptjs.compareSync(
+			password,
+			validUser.password
+		);
+
+		if (!isMatchedPassword) {
+			return next(errorHandler(404, "Email or Password are incorrect"));
+		}
+
+		// generate token
+		const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+		res.cookie("access_token", token, {
+			httpOnly: true,
+		});
+
+		// remove password
+		const { password: pss, ...rest } = validUser._doc;
+
+		res.status(200).json(rest);
 	} catch (error) {
 		next(error);
 	}
