530abf8730eba9a95c626f5aa48c024ead354a84
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-13 07:44:00 +0700

    add dark mode functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,14 +6,17 @@ import { BrowserRouter } from "react-router-dom";
 import store, { persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<PersistGate persistor={persistor}>
 			<Provider store={store}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
+				<ThemeProvider>
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+				</ThemeProvider>
 			</Provider>
 		</PersistGate>
 	</React.StrictMode>
