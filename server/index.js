ea0a7371b758437a2ddd391da9b53679b9a9fd28
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-14 15:27:50 +0700

    create get posts api route

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ app.listen(3000, () => {
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
 	const message = err.message || "Internal Server Error";
+	console.log(err.stack);
 	res.status(statusCode).json({
 		success: false,
 		statusCode,
