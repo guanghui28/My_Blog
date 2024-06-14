337d1b870871f28f6abb05e5b6317ec2659d649e
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-14 21:00:23 +0700

    call to action

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-
+import CallToAction from "../components/CallToAction";
 const PostPage = () => {
 	const { postSlug } = useParams();
 	const [post, setPost] = useState(null);
@@ -68,7 +68,11 @@ const PostPage = () => {
 				<div
 					className="p-3 max-w-2xl mx-auto w-full post-content"
 					dangerouslySetInnerHTML={{ __html: post.content }}
-				></div>
+				/>
+
+				<div className="max-w-4xl mx-auto w-full">
+					<CallToAction />
+				</div>
 			</main>
 		)
 	);
