4cbcbd8b64995bfe4777375b082fa7620958f41f
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-19 16:11:22 +0700

    add pre install

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import path from "path";
 configDotenv();
+
+const __dirname = path.resolve();
 
 mongoose
 	.connect(process.env.MONGO_URI)
@@ -21,11 +24,16 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
